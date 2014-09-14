4e8f07a7c0fc535fdebb24041fbdabb0794eaa0a
Jason Lo <mr.lo.mhs@gmail.com>
2014-09-14 12:54:46 -0700

    reset to blank for gopher snakes

--- controller.rb
+++ controller.rb
@@ -1,39 +1,4 @@
 require 'sinatra'
 require './models/coach'
 
-get '/' do
-  @coaches = Coach.all
-  erb :index
-end
 
-get '/coaches/new' do
-  erb :new
-end
-
-get '/coaches/:id/edit' do
-  @coach = Coach.find(params[:id].to_i)
-  erb :edit
-end
-
-get '/coaches/:id' do
-  @coach = Coach.find(params[:id].to_i)
-  erb :show
-end
-
-
-post '/coaches' do
-  puts params
-  Coach.create(params)
-  redirect '/'
-end
-
-put '/coaches/:id' do
-  Coach.update(params)
-  redirect '/'
-end
-
-delete '/coaches/:id' do
-  @coach = Coach.find(params[:id].to_i)
-  @coach.destroy
-  redirect '/'
-end
