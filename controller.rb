cb7b448fc6e596fe40475af26deaba57031dd742
Jason Lo <mr.lo.mhs@gmail.com>
2014-09-14 14:47:10 -0700

    second half

--- controller.rb
+++ controller.rb
@@ -8,12 +8,37 @@ get '/' do
   #embedded ruby
 end
 
+
+
+get '/coaches/new' do
+  erb :new
+end
+
+get '/coaches/:id/edit' do
+  @coach = Coach.find(params[:id].to_i)
+  @id = Coach.get_id(@coach)
+  erb :edit
+end
+
+put '/coaches/:id' do
+  coach = Coach.update({:id => params[:id], :name => params[:coach_name]})
+  redirect '/'
+end
+
 get '/coaches/:id' do
   puts params
   #params- local variable that exists for every route, contains info from the http request (HASH)
   @coach = Coach.find(params[:id].to_i)
   erb :show
 end
+
+post '/coaches' do
+  puts params
+  coach = Coach.create(params[:coach_name])
+  redirect '/'
+end
+#capybara
+
 #REST
 
 #HTTP requests
--- models/coach.rb
+++ models/coach.rb
@@ -7,4 +7,16 @@ class Coach
   def self.find(index)
     @@database[index]
   end
+
+  def self.create(name)
+    @@database << name
+  end
+
+  def self.get_id(name)
+    @@database.index(name)
+  end
+
+  def self.update(options = {})
+    @@database[options[:id].to_i] = options[:name]
+  end
 end
